f040b472d67fa59ca9dc47eb03a5c6d1fac6c3d2
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-12-15 21:58:46 +0600

    SQL practice - MySQL workbench

---
+++ 09-in/in-2.sql
@@ -0,0 +1,10 @@
+SELECT * FROM car.цена
+WHERE Car IN('Mazda','BMW')
+
+/* 8	Mazda	CX-9	 38 800 
+9	Mazda	CX-5	 23 000 
+10	Mazda	3	 18 000 
+11	Mazda	6	 17 000 
+12	BMW	M8	 150 000 
+13	BMW	X5	 145 000 
+14	BMW	M4	 100 000  */
---
+++ 09-in/in-3.sql
@@ -0,0 +1,8 @@
+SELECT * FROM car.цена
+WHERE Code IN('1','3','5')
+
+/*
+1	Nissan	Juke	 22 000 
+3	Nissan	Patrol	 53 300 
+5	Nissan	Sentra	 15 000 
+*/
